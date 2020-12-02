ae0f5d7bda21937b9433cd5bbcf0d57f7e09fbec
Vinicius Stelet <vinicius.bootcamp@gmail.com>
2020-12-02 18:16:26 -0300

    db seed car and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,20 @@ puts "Database deleted"
 
 puts "Seeding database"
 
-100.times do
- car = Car.create!(
-        user_id: rand(1..3),
+5.times do
+  user = User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    phone: Faker::PhoneNumber.cell_phone,
+    address: Faker::Address.full_address,
+    email: Faker::Internet.email,
+    password: '123456',
+    password_confirmation: '123456'
+    )
+
+  5.times do
+    car = Car.create!(
+        user_id: user.id ,
         brand: Faker::Vehicle.manufacture,
         model: Faker::Vehicle.model,
         year: Faker::Vehicle.year,
@@ -25,6 +36,7 @@ puts "Seeding database"
         city: Faker::Address.full_address
       )
   puts "Created car #{car.brand} - #{car.model} - #{car.year} - #{car.km}km"
+end
 end
 
 puts "Created #{Car.count} cars"
