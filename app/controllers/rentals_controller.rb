2c5be9a15f2907738ab09ca3417daf14cd23c6c6
Vinicius Stelet <vinicius.bootcamp@gmail.com>
2020-12-03 17:34:40 -0300

    Added model Rental, routes, controllers, and views show,update,edit

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,56 @@
 class RentalsController < ApplicationController
+  before_action :set_rental, only: [ :show, :edit, :update ]
+  before_action :set_car, only: [ :show, :new, :create, :edit, :update ]
+
+  def index
+    @rentals = Rental.where(car_id: params[:car_id])
+  end
+
+  def show
+  end
+
+  def new
+    @rental = Rental.new
+  end
+
+  def create
+    @rental = Rental.new(rental_params)
+    @rental.car = @car
+    @rental.user = current_user
+
+    if @rental.save!
+      redirect_to car_rental_path(@car, @rental)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @rental.update(rental_params)
+      redirect_to car_rental_path(@car, @rental)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:start_date, :finish_date, :user_id, :car_id)
+  end
+
+  def set_rental
+    # @rental = Rental.find(params[:id])
+    @rental = Rental.find(params[:id])
+  end
+
+  def set_car
+    @car = Car.find(params[:car_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cars do
-    resources :rentals, only: [:new, :create, :index]
+    resources :rentals
     resources :reviews, only: [:new, :create, :index]
   end
 end
