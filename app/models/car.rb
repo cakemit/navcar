ada18cb82f94c79a02d5b3ef0d59845062e20424
Vinicius Stelet <vinicius.bootcamp@gmail.com>
2020-12-03 13:58:08 -0300

    Implemented car models validation and edit and update cars views

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,5 @@
 class Car < ApplicationRecord
   belongs_to :user
-  has_many :rentals
+  has_many :rentals, dependent: :destroy
+  validates :brand, :model, :year, :km, :city, :user_id, presence: true
 end
