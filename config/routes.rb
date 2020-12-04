1cdab213037865e20c52fb05010f0c62bd8a768b
luis <luishenriquesimoes@yahoo.com.br>
2020-12-04 13:09:14 -0300

    changes in navbar and footer, links and routes to static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'about_us', to: 'pages#about_us', as: :about_us
+  get 'blogs', to: 'pages#blogs', as: :blogs
 
   devise_for :users
 
   resources :cars do
     resources :rentals
-    resources :reviews, only: [:new, :create, :index]
+    resources :reviews, only: %i[new create index]
   end
   resources :rentals, only: :index
 end
