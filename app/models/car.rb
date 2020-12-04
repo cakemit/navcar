c63ccb51a6db397fd79c2fea973330c6dcf780c5
Vinicius Stelet <vinicius.bootcamp@gmail.com>
2020-12-04 16:43:43 -0300

    Configuration active_storage-cloudinary enviroment and config files; models has_attached photo

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,7 @@
 class Car < ApplicationRecord
   belongs_to :user
   has_many :rentals, dependent: :destroy
+  has_many_attached :photos
   validates :brand, :model, :year, :km, :city, :user_id, presence: true
   validates :category, presence: true, inclusion: { in: [ "Vintage", "Luxury", "Off-road", "Sport"] }
 end
