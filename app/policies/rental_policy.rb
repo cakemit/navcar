b9935d59df12048a260d6a6b9a5e4248fb493049
Claudia Tanaka <catanaka@hotmail.com>
2020-12-05 14:17:42 -0300

    created view for My Reservations link from navbar

--- app/policies/rental_policy.rb
+++ app/policies/rental_policy.rb
@@ -1,25 +1,21 @@
 class RentalPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      # para ser uma view só do owner fazer:
+      # CORRIGIR para dar acesso para user que está alugando e car owner:
       scope.where(user: user)
     end
   end
 
   # qualquer user pode criar new car rental
+  # como bloquear se o user for car owner? só na view?
   def create?
-    record.user == user && record.car.user != user
+    true
   end
 
   # customer can view his own rental
-  # car owner can view his car's rental
+  # car owner can view his car's rentals
   def show?
     record.user == user || record.car.user == user
-  end
-
-  # customer can edit his own rental
-  def update?
-    record.user == user
   end
 
   # customer can cancel his own rental
