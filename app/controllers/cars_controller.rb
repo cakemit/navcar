8c8b8b1cbae27fc2fedeec539ef151d8e9ae8b0b
Claudia Tanaka <catanaka@hotmail.com>
2020-12-05 09:36:56 -0300

    fixed car policy and GET/car/:id so that owner cant rent his own car

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,15 +2,18 @@ class CarsController < ApplicationController
   before_action :set_car, only: %i[show edit update destroy]
   before_action :set_user
 
+  # GET cars_path HAS A VIEW
   def index
     @cars = policy_scope(Car).order(created_at: :desc)
   end
 
+  # GET new_car_path HAS A VIEW
   def new
     @car = Car.new
     authorize @car
   end
 
+  # POST cars_path
   def create
     @car = Car.new(car_params)
     authorize @car
@@ -21,10 +24,13 @@ class CarsController < ApplicationController
     end
   end
 
+  # GET edit_car_path HAS A VIEW
   def edit
     authorize @car
   end
 
+  # PATCH car_path
+  # PUT car_path
   def update
     authorize @car
     if Car.update(car_params)
@@ -34,10 +40,12 @@ class CarsController < ApplicationController
     end
   end
 
+  # GET car_path HAS A VIEW
   def show
     authorize @car
   end
 
+  # DELETE car_path
   def destroy
     authorize @car
     @car.destroy
