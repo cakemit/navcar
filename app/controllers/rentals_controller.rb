c635be50643a1b104dd1b3f569ff1cff114ef8a8
Claudia Tanaka <catanaka@hotmail.com>
2020-12-07 15:25:20 -0300

    tira fundo bege dos cards e fix price to currency format

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -10,8 +10,6 @@ class RentalsController < ApplicationController
   
   # GET car_rental_path '/cars/:car_id/rentals/:id' VIEW
   def show
-    time = @rental.finish_date - @rental.start_date
-    @price = time * @rental.car.daily_rate
     authorize @rental
   end
 
@@ -27,6 +25,8 @@ class RentalsController < ApplicationController
     @rental = Rental.new(rental_params)
     @rental.car = @car
     @rental.user = current_user
+    days = @rental.finish_date - @rental.start_date
+    @amount = days * @rental.car.daily_rate
     authorize @rental
     if @rental.save
       redirect_to car_rental_path(@car, @rental)
