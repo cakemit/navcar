8d1b1af455e0f71a98bf9c5c8c2b6b88a56fdbc5
Vinicius Stelet <vinicius.bootcamp@gmail.com>
2020-12-08 14:17:46 -0300

    Implementado mapa na index de cars

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,10 +29,12 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import { initFlatPickr } from "../plugins/flatpickr";
+import { initMapbox } from '../plugins/init_mapbox';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initAutocomplete();
   initFlatPickr();
+  initMapbox();
 });
